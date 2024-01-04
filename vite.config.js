8d2899b549574cee90f00ca64b419b1809ae9fbd
Zach Caldwell <24858526+caldwellz@users.noreply.github.com>
2024-01-04 16:52:30 -0700

    FE: Change vendor chunking and add API_BASE

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const platformTargets = ['firefox120', 'edge88', 'safari14'];
+let codeLen = 0;
 
 export default defineConfig(({ command, mode, isPreview }) => {
-    const developmentMode = mode !== 'production';
+    const devMode = mode !== 'production';
     const building = command !== 'serve' && !isPreview;
     return {
         root: './frontend',
@@ -17,14 +18,28 @@ export default defineConfig(({ command, mode, isPreview }) => {
             port: 8080,
             strictPort: false,
         },
-        envPrefix: ['DOTENV_', 'VITE_'],
+        envPrefix: ['API_', 'DOTENV_', 'VITE_'],
         build: {
             outDir: './dist',
             copyPublicDir: true,
             target: building ? platformTargets : 'esnext',
-            minify: developmentMode ? false : 'esbuild',
+            minify: devMode ? false : 'esbuild',
             emptyOutDir: building,
-            sourcemap: developmentMode,
+            sourcemap: devMode,
+            rollupOptions: {
+                output: {
+                    compact: !devMode,
+                    manualChunks: (id, { getModuleInfo }) => {
+                        const pathParts = id.split('/');
+                        const modIndex = pathParts.indexOf('node_modules');
+                        if (modIndex >= 0) {
+                            codeLen += getModuleInfo(id).code?.length ?? 0;
+                            return `vendor-${Math.trunc(codeLen / 1500000)}`;
+                            // return pathParts[modIndex + 1].replace('@', '') + '-' + length;
+                        }
+                    },
+                },
+            },
         },
     };
 });
